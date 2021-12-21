ae1cec2510fa92b269c568d0ed2317c05d01a08a
woojiahao <woojiahao1234@gmail.com>
2021-12-21 08:18:48 +0800

    Remove all electron-webpack files

--- src/renderer/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import entry component
-import Entry from './Entry.jsx';
-
-
-// render `Entry` component
-ReactDOM.render(React.createElement(Entry), document.getElementById('app'));
